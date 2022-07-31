e6232c65b49d05c2ae5ca31f994d4dbe599cd0a5
MrStickyPiston <97804691+MrStickyPiston@users.noreply.github.com>
2022-07-31 20:22:37 +0200

    Added form endpoint

--- src/data.js
+++ src/data.js
@@ -95,7 +95,7 @@ export const resume = null;
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["portfolio", "MrStickyPiston.github.io", "example-3"];
+export const filteredProjects = ["portfolio", "MrStickyPiston.github.io", ""];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
 export const projectCardImages = [
@@ -110,4 +110,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/xlevrbjr";
